a7f83af6776001e6a89b08be15090f709372af53
Rfroggy16 <ryannginyar@gmail.com>
2026-09-26 15:32:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Tikvah service worker - makes the app installable + offline-ready */
 
-const CACHE_NAME = "tikvah-v5";
+const CACHE_NAME = "tikvah-v6";
 const CORE_ASSETS = [
   "./",
   "./index.html",
